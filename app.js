60cb6544e085e9e270bfd4047519154a34d1f59c
anthonyng288 <anthonynguyen832@gmail.com>
2022-09-17 00:55:33 +1000

    build: add movie route

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 let moviesRouter = require('./routes/movies')
+let searchRouter = require ('./routes/search');
 
 var app = express();
 
@@ -22,7 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/movies', moviesRouter);
+app.use('/search', searchRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
