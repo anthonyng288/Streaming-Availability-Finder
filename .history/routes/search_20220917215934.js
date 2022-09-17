7d57dd298e30e4c1b02995b282eadee81f1b9e8f
anthonyng288 <anthonynguyen832@gmail.com>
2022-09-18 03:09:43 +1000

    build: add new similar route

---
+++ .history/routes/search_20220917215934.js
@@ -0,0 +1,124 @@
+const express = require('express');
+const https = require('https');
+const router = express.Router();
+const axios = require("axios");
+const { response } = require('../app');
+
+router.get('/', (req, res) => {
+    res.render('searchMovie')
+})
+
+router.post('/', (req, res) => {
+    res.redirect(`/search/${req.body.favoriteMovie}`)
+    console.log("From input" + req.body.favoriteMovie);
+})
+
+router.get("/:movieTitle", (req, res) => {
+    const searchMovieEndpoint = `https://api.themoviedb.org/3/search/movie?api_key=bbd65721f7d2491fab3bc98e879ff7d7&language=enUS&query=${req.params.movieTitle}`;
+    console.log("From movieTitle Route " + req.params.movieTitle);
+    console.log("Movie endpoint: " + searchMovieEndpoint);
+    axios   
+        .get(searchMovieEndpoint)
+        .then((response) => {
+            const {data} = response;
+            const allMovies = data.results;
+
+            const topTenMovies = allMovies.splice(0, 10);
+            // console.log(topTenMovies);
+            res.render("movieSelect", {movies: topTenMovies});
+            console.log(topTenMovies);
+        });
+        // .catch((error) => {
+        //     console.log(erro
+        // });
+})
+
+router.get("/movie/:movieID", (req, res) => {
+    const movieEndpoint = `https://api.themoviedb.org/3/movie/#{req.params.movieID}?api_key=bbd65721f7d2491fab3bc98e879ff7d7&language=en-US`
+    console.log(movieEndpoint);
+});
+
+
+
+
+
+// router.get('/:movieTitle', (req, res) => {
+//     const options = createMovieOptions(req.params.movieTitle);//Await/promise here?
+//     //console.log(options)
+//     const movieReq = https.request(options, (movieRes) => {
+//             //Callback function, deals with what we get 
+//             let body = [];
+//             movieRes.on('data', function(chunk){
+//                 body.push(chunk);
+//             })
+//             movieRes.on('end', function() {
+//                 res.writeHead(movieRes.statusCode,{'content-type': 'text/html'});
+//                 const bodyString = body.join('');
+//                 const rsp = JSON.parse(bodyString);   
+//                 //console.log(rsp.results[0].poster_path);  
+//                 const s = createPage(rsp);
+//                 res.write(s);
+//                 res.end()
+//             });
+
+//     })
+//     movieReq.on('error', (e) => {
+//         console.log(e);
+//     })
+//     movieReq.end();
+// });
+
+const tmdb = {
+    api_key: 'bbd65721f7d2491fab3bc98e879ff7d7',
+    language: 'enUS',
+    page: 1,
+    include_adult: true,
+    region: 'AU',
+}
+
+function createMovieOptions(query){
+    const options = {
+        hostname: 'api.themoviedb.org',
+        port: 443,
+        path: '/3/search/movie?',
+        method: 'GET'
+    }
+    const str = "api_key=" + tmdb.api_key + 
+                "&language=" + tmdb.language +
+                "&query=" + query + 
+                "&page=" + tmdb.page + 
+                "&include_adult=" + tmdb.include_adult + 
+                "&region=" + tmdb.region;
+    options.path += str;
+    return options;
+}
+
+function parseMovieRsp(rsp){
+    let s = "";
+    for (let i = 0; i < rsp.results.length; i++){
+        movie = rsp.results[i];
+        posterUrl = `https://image.tmdb.org/t/p/original${movie.poster_path}`
+        movieDetailsUrl = `https://api.themoviedb.org/3/movie/${movie.id}?api_key=bbd65721f7d2491fab3bc98e879ff7d7&language=en-US`
+        s+= `<a href="${movieDetailsUrl}"><img alt="${movie.title}" src="${posterUrl}"/></a>`;
+        // console.log(movie.original_title);
+        //console.log(s);
+        //console.log(movieDetailsUrl);
+    }
+    console.log(s);
+    return s;
+}
+function createPage(rsp){
+    const number = rsp.results.length;
+    const imageString = parseMovieRsp(rsp);
+    //console.log("This is the image string " + imageString);
+    const str = '<!DOCTYPE html>' +
+    '<html><head><title>Movie JSON</title></head>'+
+       ' <body>' +        
+       ' <h1>Movies</h1>' +        
+        'Total number of entries is:' + number +'</br>'      
+        + imageString +          
+       ' </body></html>';
+    return str;
+}
+
+module.exports = router; 
---
+++ .history/routes/search_20220918015034.js
@@ -0,0 +1,89 @@
+const express = require('express');
+const router = express.Router();
+const axios = require("axios");
+
+
+
+router.get('/', (req, res) => {
+    res.render('searchMovie')
+})
+
+router.post('/', (req, res) => {
+    res.redirect(`/search/${req.body.favoriteMovie}`)
+    console.log("From input" + req.body.favoriteMovie);
+})
+
+router.get("/:movieTitle", (req, res) => {
+    const searchMovieEndpoint = `https://api.themoviedb.org/3/search/movie?api_key=${tmdb.api_key}&language=${tmdb.language}&query=${req.params.movieTitle}`
+    axios
+        .get(searchMovieEndpoint)
+        .then((response) => {
+            const {data} = response;
+            const allMovies = data.results;
+
+            const topTenMovies = allMovies.splice(0, 10);
+            res.render("movieSelect", {movies: topTenMovies});
+        });
+        // .catch((error) =>=df
+        //     console.log(err
+        // }
+})
+
+router.get("/movie/:movieID", (req, res) => {
+    const movieEndpoint = `https://api.themoviedb.org/3/movie/${req.params.movieID}?api_key=bbd65721f7d2491fab3bc98e879ff7d7&language=en-US`
+    console.log("This is the movieEndpoint " + movieEndpoint);
+    const similarMovieEndpoint = `https://api.themoviedb.org/3/movie/${req.params.movieID}/similar?api_key=${tmdb.api_key}&language=${tmdb.language}&page=1`
+
+    axios
+        .get(similarMovieEndpoint)
+        .then(similarResponse = (sim_response) => {
+            const {similarMovieData} = sim_response;
+            const allSimilarMovies = similarMovieData.results;
+            console.log(allSimilarMovies);
+            const topTenSimilarMovies = allSimilarMovies.splice(0,10);
+            //console.log(topTenSimilarMovies);
+            // res.render("similarEndpoint", {similarMovies: topTenSimilarMovies})
+            return topTenSimilarMovies;
+
+        });
+
+    axios
+    .get(movieEndpoint)
+    .then(movieResponse = (response) => {
+        const {data} = response;
+        const movieDetails = data;
+        console.log(movieDetails);
+        return movieDetails
+    });
+    res.render("similarMovies", {movies: movieResponse(), similarMovies: similarResponse()});
+});
+
+// router.param("movieID", async (req, res, next, movieID) => {
+//     //console.log(movieID);
+//     const similarMovieEndpoint = `https://api.themoviedb.org/3/movie/${movieID}/similar?api_key=${tmdb.api_key}&language=${tmdb.language}&page=1`
+
+//     //give me similar movies, then use that to print everything
+//     //console.log("This is the similar movie endpoint" + similarMovieEndpoint);
+//     axios
+//         .get(similarMovieEndpoint)
+//         .then((response) => {
+//             const {data} = response;
+//             const allSimilarMovies = data.results;
+//             const topTenSimilarMovies = allSimilarMovies.splice(0,10);
+//             console.log(topTenSimilarMovies);
+//             res.render("similarEndpoint", {similarMovies: topTenSimilarMovies})
+
+//         });
+//     next();
+// })
+
+const tmdb = {
+    api_key: 'bbd65721f7d2491fab3bc98e879ff7d7',
+    language: 'enUS',
+    page: 1,
+    include_adult: true,
+    region: 'AU',
+}
+
+
+module.exports = router; 
